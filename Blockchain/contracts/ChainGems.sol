1d444c7032cf69ff1fb8c4b318fbba59590ca099
dario <dariogabalec@gmail.com>
2023-03-26 10:11:13 -0300

    feat: created ERC1155 contract

---
+++ Blockchain/contracts/ChainGems.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract ChainGems is ERC1155, Ownable {
+    uint256 public constant NATIVE_TOKEN = 0;
+
+    constructor() ERC1155('') {
+        _mint(msg.sender, NATIVE_TOKEN, 10 ** 18, '');
+    }
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public onlyOwner {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
