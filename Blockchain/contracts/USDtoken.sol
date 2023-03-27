a70284315386e78abe05c17b61b3baae5665e6f7
dario <dariogabalec@gmail.com>
2023-03-26 22:39:27 -0300

    feat: created ERC20 token for testing

---
+++ Blockchain/contracts/USDtoken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+/**
+ * @title An ERC20 contract for USD tokens. Only used for testing.
+ * @author @dario13
+ * @notice This contract creates a USD token.
+ */
+contract USDtoken is ERC20 {
+    constructor() ERC20('USDtoken', 'USD') {
+        _mint(msg.sender, 10 * 18 ** decimals());
+    }
+}
