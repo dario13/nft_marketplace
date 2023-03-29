957cc373090e06da3a1fd47be814e729c1bf5db9
dario <dariogabalec@gmail.com>
2023-03-29 19:16:22 -0300

    feat: created wallet store

---
+++ Frontend/src/store/wallet/wallet.store.ts
@@ -0,0 +1,52 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+
+export type Wallet = {
+  readonly isWalletInstalled: boolean
+  readonly isAccountConnected: boolean
+  readonly isAccountLoggedOut: boolean
+  readonly signer?: SignerWithAddress | undefined
+}
+
+export type WalletStoreState = Wallet & {
+  setWalletInstalled: (isWalletInstalled: boolean) => void
+  setAccountConnected: (isAccountConnected: boolean) => void
+  setAccountLoggedOut: (isAccountLoggedOut: boolean) => void
+  setSigner: (signer: SignerWithAddress | undefined) => void
+  disconnect: () => void
+}
+
+const initialState: Wallet = {
+  isWalletInstalled: false,
+  isAccountConnected: false,
+  isAccountLoggedOut: false,
+  signer: undefined,
+}
+
+const useWalletStore = create<WalletStoreState>()(
+  persist(
+    (set) => ({
+      ...initialState,
+      setWalletInstalled: (isWalletInstalled: boolean) =>
+        set((state) => ({ ...state, isWalletInstalled })),
+      setAccountConnected: (isAccountConnected: boolean) =>
+        set((state) => ({ ...state, isAccountConnected })),
+      setAccountLoggedOut: (isAccountLoggedOut: boolean) =>
+        set((state) => ({ ...state, isAccountLoggedOut })),
+      setSigner: (signer: SignerWithAddress | undefined) => set((state) => ({ ...state, signer })),
+      disconnect: () =>
+        set({ isWalletInstalled: true, isAccountConnected: false, isAccountLoggedOut: true }),
+    }),
+    {
+      name: 'wallet',
+      partialize: (state) => ({
+        isWalletInstalled: state.isWalletInstalled,
+        isAccountConnected: state.isAccountConnected,
+        isAccountLoggedOut: state.isAccountLoggedOut,
+      }),
+    },
+  ),
+)
+
+export { useWalletStore }
