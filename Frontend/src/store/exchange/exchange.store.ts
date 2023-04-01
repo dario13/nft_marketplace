8d6ebce8a6719eedb72761ab5b76f14c40f09aa5
dario <dariogabalec@gmail.com>
2023-04-01 19:08:49 -0300

    feat: created store for exchange

--- Frontend/src/store/exchange/exchange.store.ts
+++ Frontend/src/store/exchange/exchange.store.ts
@@ -2,18 +2,14 @@ import { create } from 'zustand'
 
 const initialState = {
   coinsBalance: '0',
-  transactionInProgress: false,
 }
 
 type ExchangeState = {
   coinsBalance: string
-  transactionInProgress: boolean
   setCoinsBalance: (coinsBalance: string) => void
-  setTransactionInProgress: (transactionInProgress: boolean) => void
 }
 
 export const useExchangeStore = create<ExchangeState>((set) => ({
   ...initialState,
   setCoinsBalance: (coinsBalance: string) => set({ coinsBalance }),
-  setTransactionInProgress: (transactionInProgress: boolean) => set({ transactionInProgress }),
 }))
