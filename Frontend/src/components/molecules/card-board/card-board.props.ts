10973f40238a078794d1bcc7b8c91ef2a8a8007d
dario <dariogabalec@gmail.com>
2023-04-01 18:52:47 -0300

    refactor: removed unused component

--- Frontend/src/components/molecules/card-board/card-board.props.ts
+++
@@ -1,6 +0,0 @@
-import { AnimatedCardProps } from '@/components/atoms/animated-card/animated-card.props'
-
-export type CardBoardProps = {
-  addCard: (card: AnimatedCardProps) => void
-  resetBoard: () => void
-}
